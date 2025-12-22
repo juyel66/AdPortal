7367380647a04cb853b0314bc978c7457b442b2a
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2025-12-23 04:27:23 +0600

    Feat(Home): Home page update

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,10 @@
-import { Button } from "@/components/ui/button";
-import { Link } from "react-router";
+import Features from "./Features.tsx/Features";
 
 
 const Home = () => {
     return (
         <div className=" ">
-   
-   <div>
-   <Link to="/user-dashboard/dashboard"> <Button  >Go to Dashboard</Button></Link>
-   </div>
+            <Features />
 
             
         </div>
