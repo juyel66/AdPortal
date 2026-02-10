d5a335353d03111e8534f03b1380e763982f396e
Md Juyel Rana <juyelrana@joinventureai.com>
2026-02-11 02:34:04 +0600

    feat(store): added the team store

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+
 import authReducer from "./features/auth/authSlice";
+import teamReducer from "./features/Team/teamSlice";
+// import teamReducer from "";
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
+    team: teamReducer,
   },
 });
 
