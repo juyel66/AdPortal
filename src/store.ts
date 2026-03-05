eddba696b0369d8e90d07e6c2b6107af8db423ba
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-03-06 04:15:11 +0600

    feat(Store): add notifications reducer to centralize notification state management

--- src/store.ts
+++ src/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import authReducer from "./features/auth/authSlice";
 import teamReducer from "./features/Team/teamSlice";
+import notificationsReducer from "./features/auth/Context/notificationsSlice";
 
 
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     team: teamReducer,
+    notifications: notificationsReducer,
   },
 });
 
