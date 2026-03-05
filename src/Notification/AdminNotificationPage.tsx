8d804099d585c967341f5cf2325c5099e9e32803
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-03-06 04:15:33 +0600

    feat(AdminNotificationPage): create Admin Notification page component

---
+++ src/Notification/AdminNotificationPage.tsx
@@ -0,0 +1,12 @@
+
+
+const AdminNotificationPage = () => {
+    return (
+        <div>
+            <h1>Admin Notifications</h1>
+            
+        </div>
+    );
+};
+
+export default AdminNotificationPage;
