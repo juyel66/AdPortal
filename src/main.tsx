1a4cbedad6cf248dc573d7ee18d8c99dc0431991
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-03-06 04:08:15 +0600

    feat(Routing): replace RouterProvider with RootWraper for improved routing structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,21 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { RouterProvider } from "react-router-dom";
+
 
 import "./index.css";
-import { router } from "./Router";
+
 import { store } from "./store";
 import { Toaster } from "./components/ui/sonner";
 import { CampaignProvider } from "../src/pages/create-campaign/CampaignContext"; 
+import { RootWraper } from "./wrapper";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
       <CampaignProvider> 
-        <RouterProvider router={router} />
+        <RootWraper />
+
         <Toaster position="top-center" />
       </CampaignProvider> 
     </Provider>
