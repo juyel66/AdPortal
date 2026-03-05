43166785183a8e1f57eeb29098c64e9eda801bfe
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2026-03-06 04:15:48 +0600

    refactor(AdminNavbar): clean up code structure and improve readability

--- src/pages/admin/AdminNavbar/AdminNavbar.tsx
+++ src/pages/admin/AdminNavbar/AdminNavbar.tsx
@@ -1,48 +1,40 @@
-
 import { useUserProfile } from "@/hooks/useUserProfile";
+import NotificationBell from "@/Notification/NotificationBell";
 import { Link } from "react-router";
 
 
 const AdminNavbar = () => {
-
-    // const {user } = useAppSelector((state) => state.auth);
-
-    const {full_name, email} = useUserProfile();
-
-
-
-    
-
-
+    const { full_name, email } = useUserProfile();
 
     return (
         <div>
-
-            <div className="navbar bg-base-100  border-b-2    ">
-  <div className="navbar-start">
-    <div className="dropdown">
-     
-      
-    </div>
-    
-  </div>
-  <div className="navbar-center hidden lg:flex">
-
-  </div>
-  <div className="navbar-end">
-  <div className='flex items-center gap-4'>
-    <Link to="notification">
-        <img className='w-10 h-10 rounded-full' src="https://res.cloudinary.com/dqkczdjjs/image/upload/v1766353699/Button_3_jqwyyc.png" alt="" />
-    </Link>
-     <div>
-    <p className='text-xl'>{full_name}</p>
-    <p className='text-gray-500'>{email}</p>
-   </div>
-  </div>
-  </div>
-</div>
-
-            
+            <div className="navbar bg-base-100 border-b-2">
+                <div className="navbar-start">
+                    <div className="dropdown"></div>
+                </div>
+                
+                <div className="navbar-center hidden lg:flex"></div>
+                
+                <div className="navbar-end">
+                    <div className='flex items-center gap-4'>
+                        
+                        <NotificationBell />
+                        
+                        {/* <Link to="notification">
+                            <img 
+                                className='w-10 h-10 rounded-full' 
+                                src="https://res.cloudinary.com/dqkczdjjs/image/upload/v1766353699/Button_3_jqwyyc.png" 
+                                alt="notification" 
+                            />
+                        </Link> */}
+                        
+                        <div>
+                            <p className='text-xl'>{full_name}</p>
+                            <p className='text-gray-500'>{email}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
